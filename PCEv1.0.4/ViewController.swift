f5034b220a0458978dd115daaac950564e567691
Haoqiang Wang <davidwhq123@gmail.com>
2016-04-07 22:38:34 +0800

    add some picture

---
+++ PCEv1.0.4/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PCEv1.0.4
+//
+//  Created by 王浩强 on 16/3/15.
+//  Copyright © 2016年 王浩强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
