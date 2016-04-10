aa5f6b90534731da0f79ea4c838deb22ca1b9061
Kevin_Feng <Kevin_Feng@139.com>
2016-04-10 16:11:32 +0800

    i finished the function of filter completely. but UI i still not do it. When the designer give me the design i will do it.

--- Codes/PCEv1.0.4/cameraPeople.swift
+++ Codes/PCEv1.0.4/cameraPeople.swift
@@ -13,35 +13,49 @@ import AssetsLibrary
 import CoreMotion
 var Score = 0.0
 class cameraPeople: UIViewController , AVCaptureVideoDataOutputSampleBufferDelegate{
+    //界面控件
     @IBOutlet weak var cameraScoreLabel: UILabel!//打分类 显示分数
     @IBOutlet weak var cameraUIView: UIImageView!//显示图片
     @IBOutlet var filterButtonContainer: UIView!// 滤镜容器
     @IBOutlet var cameraBackButton: UIButton!//返回按钮
     @IBOutlet var cameraRecordsButton: UIButton!//拍照按钮
+    //摄像头相关
     var cameraCaptureSession:AVCaptureSession!//拍照序列
-    var cv = opencv()//cv类
-    var filter:CIFilter!
     lazy var cameraCIContext: CIContext = {
         let eaglContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
         let options = [kCIContextWorkingColorSpace : NSNull()]
         return CIContext(EAGLContext: eaglContext, options: options)
     }()//拍照用的
+    var cameraCIImage:CIImage!//拍照用的
+    //opencv相关
+    var cv = opencv()//cv类
+    //滤镜相关
+    var filter:CIFilter!
+    var filterIsOpen=false//判断是否点开滤镜
     lazy var filterNames: [String] = {
         return ["CIColorInvert","CIPhotoEffectMono","CIPhotoEffectInstant","CIPhotoEffectTransfer"]
     }()//滤镜库
-    var cameraCIImage:CIImage!//拍照用的
+    
+    //时间相关
     var counter = 0;
     var timer:NSTimer!
     let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
     
-    //加速度传感器
+    //加速度传感器相关
     var cmm:CMMotionManager!
     var accelerationX:CGFloat!
     var accelerationY:CGFloat!
     var accelerationZ:CGFloat!
     
-    //图片存储单例类
+    //图片存储
     var CPhoto:cameraPhoto!
+    
+    //函数
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //界面初始化
     override func viewDidLoad() {//界面加载完成时调用
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -66,43 +80,58 @@ class cameraPeople: UIViewController , AVCaptureVideoDataOutputSampleBufferDeleg
                 self.accelerationX = CGFloat(acceleration.x)
                 self.accelerationY = CGFloat(acceleration.y)
                 self.accelerationZ = CGFloat(acceleration.z)
-//                print("x:\(self.accelerationX) ")
-//                print("y:\(self.accelerationY) ")
-//                print("z:\(self.accelerationZ) ")
+                //                print("x:\(self.accelerationX) ")
+                //                print("y:\(self.accelerationY) ")
+                //                print("z:\(self.accelerationZ) ")
             })
         }
         else{
             print("un")
         }
         filter=CIFilter(name: "CIPhotoEffectTransfer")
+        filterIsOpen=false
     }
     override func viewWillDisappear(animated: Bool) {
         if cmm.accelerometerActive{
             cmm.stopAccelerometerUpdates()
         }
     }
-
+    
+    func openCamera(){//启动相机
+        cameraCaptureSession.startRunning()
+    }
+    //触控相关
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let point = touches.first?.locationInView(self.view)
-        print(point)
-        self.cv.change_selection(point!)
+        if(Bool(filterIsOpen)){
+            filterButtonContainer.hidden=true
+            filterIsOpen=false
+        }
+        else{
+            let point = touches.first?.locationInView(self.view)
+            print(point)
+            self.cv.change_selection(point!)
+        }
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func takePicture(sender: UIButton) {//模拟相机动作
+        sender.enabled = false
+        self.cameraCaptureSession.stopRunning()
+        sleep(1)
+        CPhoto.saveImage(self.cameraUIView.image!)
+        sender.enabled = true
+        self.cameraCaptureSession.startRunning()
     }
-//    @IBAction func switchFilters(sender: AnyObject) {
-//    }
+    //滤镜操作
     @IBAction func openFilters(sender: AnyObject) {
         filterButtonContainer.hidden=false
+        filterIsOpen=true
     }
-    @IBAction func closeFilters(sender: AnyObject) {
-        filterButtonContainer.hidden=true
-    }
+    
     @IBAction func applyFilter(sender: UIButton) {//使用滤镜
         var filterName = filterNames[sender.tag]
         filter = CIFilter(name: filterName)
     }
+    
     func updateCounter(){//更新计时器
         counter += 1
     }
@@ -129,17 +158,8 @@ class cameraPeople: UIViewController , AVCaptureVideoDataOutputSampleBufferDeleg
         cameraDataOutput.setSampleBufferDelegate(self ,queue: cameraQueue)
         cameraCaptureSession.commitConfiguration()//??????
     }
-    func openCamera(){//启动相机
-        cameraCaptureSession.startRunning()
-    }
-    @IBAction func takePicture(sender: UIButton) {//模拟相机动作
-        sender.enabled = false
-        self.cameraCaptureSession.stopRunning()
-        sleep(1)
-        CPhoto.saveImage(self.cameraUIView.image!)
-        sender.enabled = true
-        self.cameraCaptureSession.startRunning()
-    }
+    
+    //打分算法
     func  captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {//视频流监测
         let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
         let formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer)!
